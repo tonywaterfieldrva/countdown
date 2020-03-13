67a7034d511ecd5f74ef38655758deb7c2843dfe
Tony Waterfield <tonywaterfieldrva@gmail.com>
2020-03-12 23:20:48 -0400

    updating countdown clock

--- assets/javascript/localjs.js
+++ assets/javascript/localjs.js
@@ -3,7 +3,7 @@ function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24) + 1;
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
     'total': t,
@@ -23,14 +23,12 @@ function initializeClock(id, endtime) {
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    console.log(t.days);
-    console.log(t.hours);
-    console.log(t.seconds);
+
     daysSpan.innerHTML = t.days;
     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    
+
     if (t.total <= 0) {
       clearInterval(timeinterval);
     }
@@ -39,7 +37,7 @@ function initializeClock(id, endtime) {
   updateClock();
   var timeinterval = setInterval(updateClock, 1000);
 }
-var deadline = '3/21/2020 17:30:00';
+var deadline = '03/21/2020 17:30:00';
 //var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
 initializeClock('clockdiv', deadline);
 
